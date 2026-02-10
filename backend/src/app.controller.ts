91ac1a3f41ceee0b0e1abcdff2c2749ff2155ec1
LJacobDev <LJacobDev@gmail.com>
2026-02-10 02:00:14 -0500

    Connect Vue build and Nest Static Serve
     backend/public serves Vue static assets

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -5,8 +5,9 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+// Commenting these out to allow Vue static serve to appear
+//  @Get()
+//  getHello(): string {
+//    return this.appService.getHello();
+//  }
 }
