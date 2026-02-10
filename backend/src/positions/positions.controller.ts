95763735773585ad461b547d42cd72b3714eeb17
LJacobDev <LJacobDev@gmail.com>
2026-02-10 02:02:43 -0500

    Set API prefix to positions

--- backend/src/positions/positions.controller.ts
+++ backend/src/positions/positions.controller.ts
@@ -3,7 +3,7 @@ import { PositionsService } from './positions.service';
 import { CreatePositionDto } from './dto/create-position.dto';
 import { UpdatePositionDto } from './dto/update-position.dto';
 
-@Controller('positions')
+@Controller('api/positions')
 export class PositionsController {
   constructor(private readonly positionsService: PositionsService) {}
 
