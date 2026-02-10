588646ba370e1c5f67e3b58fd881549993333527
LJacobDev <LJacobDev@gmail.com>
2026-02-10 02:08:20 -0500

    Set up sqlite database in positions service

--- backend/src/positions/positions.service.ts
+++ backend/src/positions/positions.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { CreatePositionDto } from './dto/create-position.dto';
 import { UpdatePositionDto } from './dto/update-position.dto';
+import Database from 'better-sqlite3';
 
 @Injectable()
-export class PositionsService {
+export class PositionsService implements OnModuleDestroy, OnModuleInit{
+  private db: Database.Database;
+
+  onModuleInit() {
+    this.db = new Database('./data/database.sqlite');
+
+    this.db.exec(`
+      CREATE TABLE IF NOT EXISTS positions (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      x INTEGER NOT NULL,
+      y INTEGER NOT NULL,
+      f TEXT NOT NULL,
+      createdAt TEXT NOT NULL
+      )
+      `)
+  }
+
+  onModuleDestroy() {
+    if (this.db)
+      this.db.close();
+  }
+
   create(createPositionDto: CreatePositionDto) {
     return 'This action adds a new position';
   }
