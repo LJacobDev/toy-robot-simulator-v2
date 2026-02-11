d9c0318da908dd982f8585cbbf4b3f988578adaa
LJacobDev <LJacobDev@gmail.com>
2026-02-11 02:53:00 -0500

    Robot design ready

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,20 +7,20 @@ const placed = ref(false);
 const currentPosition = ref(undefined);
 
 // This will show 'Report' when the player is not on the board, and will show their current co-ordinates and direction if they are on the board
-const report = computed(()=>{
+const report = computed(() => {
   return placed.value ? `${currentPosition.value.x}, ${currentPosition.value.y}, ${currentPosition.value.f} ` : 'Report'
 });
 
 const { getLatestPosition, saveCurrentPosition, deleteAllPositions } = usePositions();
 
-onMounted( async () => {
+onMounted(async () => {
 
   currentPosition.value = await getLatestPosition();
 
   console.log('app.vue log latestposition is ', currentPosition.value);
 
   if (currentPosition.value) {
-    console.log( 'there is a latest position and the robot will be assigned it')
+    console.log('there is a latest position and the robot will be assigned it')
     placed.value = true;
 
   }
@@ -28,7 +28,7 @@ onMounted( async () => {
     console.log('there is no latest position and the robot will not be on the board');
   }
 
-  
+
 
 
 });
@@ -36,23 +36,32 @@ onMounted( async () => {
 </script>
 
 <template>
+  
+  <!-- Main Viewspace -->
   <div class="game-window">
     <div class="container">
       <div class="instructions">Click to place the robot, use the buttons or arrows to move</div>
+
+      <!-- Main Grid  -->
       <div class="grid">
-          <div data-x="0" data-y="0" data-row="5" data-col="1" id="1" class="grid-tile one">0,2 (1)</div>
-          <div class="grid-tile 2">1,2 (2)</div>
-          <div class="grid-tile 3">2,2 (3)</div>
-          <!-- //you need there to be 25 cells, so that there is no overflow -->
-          <!-- so use a v-if that shows the last cell as tile if not placed and robot if placed -->
-            <!-- from there it can move around -->
-          <div id = "robot" class="grid-tile">
-              <div id="body">
-                  Body
-              </div>
-              <div id="arrow" class="east">></div>
+
+        <!-- Player Robot Character -->
+        <div id="robot-tile" class="grid-tile">
+          <div id="robot">
+            <div id="robot-head">
+              <div class="robot-eye"></div>
+              <div class="robot-eye"></div>
+            </div>
+            <div id="robot-body"></div>
           </div>
+          <div id="arrow" class="north">></div>
+        </div>
+
+        <!-- Grid Tiles -->
+        <div data-x="0" data-y="0" data-row="5" data-col="1" id="1" class="grid-tile one">0,2 (1)</div>
       </div>
+
+      <!-- Console Area (Buttons and Output) -->
       <div class="console">
         <div class="controls">
           <button>Left</button>
@@ -60,7 +69,7 @@ onMounted( async () => {
           <button>Right</button>
         </div>
         <div class="output">
-          <div class="report">{{report}}</div>
+          <div class="report">{{ report }}</div>
         </div>
       </div>
     </div>
@@ -68,9 +77,4 @@ onMounted( async () => {
 
 </template>
 
-<style scoped>
-
-
-
-
-</style>
+<style scoped></style>
